b83caa17ee04a0f2710de2c049c3b758a94d854f
unknown <mdnasifurahman@gmail.com>
2022-10-16 15:35:50 +0600

    lab 03

---
+++ lab 03/Login.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <fieldset>
+            <legend>Login</legend>
+            <table>
+                <tr>
+                    <td>Username:</td>
+                    <td><input type="text" name="Usernmae" value=""/></td>
+                </tr>
+                <tr>
+                    <td>Password:</td>
+                    <td><input type="password" name="password" value=""/></td>
+                </tr>
+                <tr>
+                    <td colspan="2"><hr></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="checkbox" name="" value=""/>Remember Me
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="" value="Submit" >
+                        <a href="ForgotPassword.html">ForgotPassword?
+                    </td>
+                </tr>
+
+            </table>
+        </fieldset>
+    </form>
+</body>
+</html>
